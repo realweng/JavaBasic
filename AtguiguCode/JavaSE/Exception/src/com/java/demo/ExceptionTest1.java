a9bfdcd788f411bece0b618e5587a1f349032dea
realweng <xingguo1997@outlook.com>
2021-01-31 15:01:14 +0800

    2021/1/31更新

---
+++ AtguiguCode/JavaSE/Exception/src/com/java/demo/ExceptionTest1.java
@@ -0,0 +1,8 @@
+package com.java.demo;/**
+* @Author：wengxingguo   
+* @Version：1.0
+* @Date：2021/1/31-14:05
+* @Since:jdk1.8
+* @Description:TODO
+*/public class ExceptionTest1 {
+}
---
+++ AtguiguCode/JavaSE/Exception/src/com/java/demo/ExceptionTest2.java
@@ -0,0 +1,8 @@
+package com.java.demo;/**
+* @Author：wengxingguo   
+* @Version：1.0
+* @Date：2021/1/31-14:37
+* @Since:jdk1.8
+* @Description:TODO
+*/public class ExceptionTest2 {
+}
