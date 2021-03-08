45953039dcb1fa08e687e6f92447e2f53c67dfc2
realweng <xingguo1997@outlook.com>
2021-03-08 21:35:05 +0800

    2021/3/8更新2

---
+++ JavaSE/MarketManager/src/com/java/project/constants/MarketConstants.java
@@ -0,0 +1,16 @@
+package com.java.project.constants;
+
+/**
+ * @Author：wengxingguo
+ * @Version：1.0
+ * @Date：2021/3/8-21:25
+ * @Since:jdk1.8
+ * @Description:TODO
+ */
+public class MarketConstants {
+    public static final String DRIVER = "com.mysql.jdbc.Driver";//驱动
+    public static final String URL = "jdbc:mysql://localhost:3306/shoppingdb?useSSL=true";//数据库地址
+    public static final String SQL_USER = "root", SQL_PASSWORD = "admin";//数据库用户名密码
+    public static final String MANAGER_ACCOUNT = "admin",MANAGER_PASSWORD = "admin";//管理员账号密码
+    
+}
