5f2550f7fab2bbebc904d9529c03d2e3c3ec6dcb
realweng <xingguo1997@outlook.com>
2021-03-24 09:09:28 +0800

    bootstrap

--- JavaWeb/HBuilderProjects/5.BootStrap/js/modal.js
+++ JavaWeb/HBuilderProjects/5.BootStrap/js/modal.js
@@ -1,36 +1,53 @@
 $(function() {
+	//新增
+	$("body").on("click", "#saveHero", function() {
+		//获取模态框中输入的值
+		var $name = $("#name").val();
+		var $life = $("#life").val();
+		var $power = $("#power").val();
 
-	$("#saveHero").click(function() {
+		//新建一行
+		var $tr = $("<tr></tr>");
+		//新建列
+		var $td1 = $("<td>" + $name + "</td>");
+		var $td2 = $("<td>" + $life + "</td>");
+		var $td3 = $("<td>" + $power + "</td>");
+		var $td4 = $(
+			'<td><button name="change" class="btn btn-primary" data-toggle="modal" data-target="#myModal2">修改</button> ' +
+			'<button name = "delete" class="btn btn-danger">删除</button></td>');
 
-		// 获取输入框的值
-		var name = $("#name").val();
-		var life = $("#life").val();
-		var power = $("#power").val();
-
-		// 创建tr
-		var $tr = $("<tr></tr>")
-
-		// 创建td   输入框的值
-		var $td1 = $("<td>" + name + "</td>");
-		var $td2 = $("<td>" + life + "</td>");
-		var $td3 = $("<td>" + power + "</td>");
-		var $td4 = $('<td><button class="btn btn-primary">修改</button>' +
-			'<button class="btn btn-danger">删除</button></td>');
-
-
-		// 将td加到tr
+		//将列添加到一行中
 		$tr.append($td1).append($td2).append($td3).append($td4);
-
-		// 将tr加到table
+		//将行添加到table中
 		$("#hero").append($tr);
-		
-		// 将数据传递到servlet完成数据添加（数据库）。。。。(ajax)
 
-		// 关闭模态框
+		//关闭模态框
 		$("#myModal").modal('hide');
-
-
-
 	});
 
-})
+	//修改--有bug
+	$("table").on("click","[name=change]",function(){
+		//获取当前的行
+		var $obj = $(this).parent().siblings();
+		$("div").on("click", "#changeHero", function() {
+			//获取模态框中的值
+			
+			var name = $("#name1").val();
+			var life = $("#life1").val();
+			var power = $("#power1").val();
+			
+			//将各项值填充到要修改的那一行
+			console.log($obj);
+			$obj[0].innerText = name;
+			$obj[1].innerText = life;
+			$obj[2].innerText = power;
+			//关闭修改模态框
+			$("#myModal2").modal('hide');
+		});
+	});
+	
+	//删除
+	$("table").on("click","[name=delete]",function(){
+		$(this).parent().parent().remove();
+	});
+});
