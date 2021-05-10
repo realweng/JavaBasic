18d02cc71c1e526b48aa52f06dfe17d63de3e2ac
realweng <xingguo1997@outlook.com>
2021-05-10 20:48:05 +0800

    日志log4j

---
+++ JavaEE/SSM-02/src/com/javasm/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.javasm.controller;
+
+import com.javasm.entity.User;
+import com.javasm.service.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+
+/**
+ * @Author：wengxingguo
+ * @Version：1.0
+ * @Date：2021/5/10-18:18
+ * @Since:jdk1.8
+ * @Description:TODO
+ */
+@Controller
+@RequestMapping("/login")
+public class UserController {
+    @Resource
+    private UserService userService;
+
+    @GetMapping
+    public String loginPage(){
+        return "base/login";
+    }
+
+    @PostMapping
+    public String login(String username, String password, HttpSession httpSession) {
+        String viewName = "base/main";
+        User user = userService.userLogin(username, password, httpSession);
+        if(user == null){
+            viewName = "base/login";
+        }
+        return viewName;
+    }
+}
